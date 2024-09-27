fc0bfe5977115e84ee0f69e05bccf218bcb3511d
Matheus-C-Machado <158229786+Matheus-C-Machado@users.noreply.github.com>
2024-09-27 17:20:19 -0300

    Add files via upload

---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocaFiltro', 'editaTarefaTemp'])
+</script>
+
+<template>
+    <form @submit.prevent="props.cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required type="text" placeholder="digite aqui a descriçaõ da tarefa " class="form-control">
+        </div>
+            <div class="col-md-2">
+                <button  type="submit"  class="btn btn-primary">Cadastrar</button>
+        </div>
+            <div class="col-md-2">
+                <select  @change="props.trocaFiltro" class="form-control">
+                    <option value="todas">Todas tarefas</option>
+                    <option value="pendentes">pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
